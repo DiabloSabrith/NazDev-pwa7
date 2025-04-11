80414064b5fe995f606a5d98e85a46254cc4bd59
abc <abc@example.com>
2025-04-11 20:47:37 +0500

    sdfs

--- src/app/start_page/page.tsx
+++ src/app/start_page/page.tsx
@@ -7,6 +7,8 @@ export default function Intensivs() {
         <div className="w-340 h-500 mx-auto containerSprint ">
               <main className="w-[95%] h-510 mx-auto flex flex-col items-center text-center  bg-[blueviolet] ">
                 <h1 className='text-7xl'>Hello</h1>
+                <p className='text-2xl text-[red]'>kcsjcmscvsxcv</p>
+
               </main>
         </div>
     )
