cbce869d38945c44caf0cdb830d0b4cb9b491911
abc <abc@example.com>
2025-04-11 20:42:17 +0500

    start page

---
+++ src/app/start_page/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import 'animate.css';
+
+
+export default function Intensivs() {
+    return(
+        <div className="w-340 h-500 mx-auto containerSprint ">
+              <main className="w-[95%] h-510 mx-auto flex flex-col items-center text-center  bg-[blueviolet] ">
+                <h1 className='text-7xl'>Hello</h1>
+              </main>
+        </div>
+    )
+}
