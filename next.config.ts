588f0ff5881ccbf299e84e989fceabd8fcc63119
abc <abc@example.com>
2025-04-16 22:02:24 +0500

    cach prod

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,6 @@
-/** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public', //  Где будут сгенерированы файлы PWA (манифест, SW)
-  register: true,   // Автоматическая регистрация service worker
-  skipWaiting: true,  //  Service worker активируется сразу
-  disable: process.env.NODE_ENV === 'development', //  Отключаем в dev-режиме
-  //  Необязательные опции (важны для production)
-  // runtimeCaching: [], //  Управление кешированием ресурсов
-  // buildExcludes: [], // Исключение файлов из сборки PWA
-})
 
 const nextConfig = {
-  reactStrictMode: true,  
-  // Другие настройки Next.js
+  reactStrictMode: true,
+  // другие настройки
 }
-
-module.exports = withPWA(nextConfig)
-
-
-
-
-
-
-
+module.exports = nextConfig
